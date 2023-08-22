aebc121ab65bf261f9a0433ce3454d0a499a8126
Mohamed Nidal <touhami9@gmail.com>
2023-08-22 18:43:08 +0100

    building app layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <div>
+      <Link to={'/'}>Fast React Pizza Co.</Link>
+
+      <p>Mohamed</p>
+    </div>
+  );
+}
